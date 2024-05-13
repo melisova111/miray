a0bd190e532e8a778c4dd0fb7dd1c9b4b095a2ba
melisova111 <aytunukmirlanovna@gmail.com>
2024-05-13 15:15:16 +0600

    worked in About

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,29 +1,45 @@
-import Title from '../../Components/Title/Title';
 import './About.css';
-import aboutImg from '../../img/about.jpg'
 
 function About() {
-  return (
-    <section className="About">
-      <Title>About Us</Title>
-      <div className="About_in flex">
-        <div className="About_img img_wrap">
-          <img src={aboutImg} alt="" />
-        </div>
-        <div className="About_txt">
-          <h3 className='About_tit'>Apple cuts greenhouse gas emissions in half</h3>
-          <p>
-            <b>Innovations in clean energy, materials, and recycling are driving progress toward Apple’s ambitious environmental goals.</b>
-            Apple has reduced its overall greenhouse gas emissions by more than 55 percent since 2015, the company shared today in its 2024 Environmental Progress Report. The milestone marks important progress on the journey toward Apple 2030, the company’s ambitious goal to become carbon neutral across its entire value chain by the end of this decade.
-          </p>
-        </div>
-      </div>
-      <div className="About_bot">
-        <p>“The proof of Apple’s commitment to climate action is in our progress: We’ve slashed emissions by more than half, all while serving more users than ever before,” said Lisa Jackson, Apple’s vice president of Environment, Policy, and Social Initiatives. “More hard work is ahead of us, and we’re focused on harnessing the power of innovation and collaboration to maximize our impact.”</p>
-        <p>Teams across Apple and its global supply chain have contributed to Apple 2030 and the company’s environmental efforts, driving innovations in clean energy and energy efficiency, materials, carbon removal, water stewardship, zero waste, and recycling. As Apple celebrates Earth Day with its customers around the world — including through curated environment-focused collections on podcasts, books, and more — here’s a look at 10 innovations, partnerships, and activities engaging customers and advancing global climate and environmental progress.</p>
-      </div>
-    </section>
-  );
+	return (
+		<>
+			<div className="container">
+				<div className="about_in flex">
+					<div className="about_left">
+						<div className="_img">
+
+						</div>
+						<div className="about_info">
+							<p className="about_info-txt">
+								Come and visit us
+							</p>
+							<a href="tel:+79998887766">+7(999)888-77-66</a>
+
+							<a href="mailto:happytummy@restaurant.com">happycosmetic@beautyshop.com </a>
+
+							<a href="geo:837 W. Marshall Lane Marshalltown, IA 50158, Los Angeles">837 W. Marshall Lane
+								Marshalltown, IA 50158, Los Angeles</a>
+						</div>
+					</div>
+					<div className="about_right">
+						<h2 className="about_tit">Welcome to our cosmetic world</h2>
+						<p className="about_txt">
+							Welcome to MirAy Cosmetics Store!
+
+							MirAy is the place where your boldest cosmetic dreams come true. Our store offers a wide range of cosmetic products designed to enhance your natural beauty and express your individuality. <br />
+
+							We provide high-quality cosmetic products that combine innovative formulas and the latest beauty trends. From vibrant pigments to subtle shades, from skincare essentials to makeup products, we have everything you need to create your perfect look. <br />
+
+							Our team of beauty experts is always ready to help you choose products that suit your needs and desires. We strive to create a pleasant atmosphere and a cozy space where every customer can feel special and inspired.
+							<br />
+							Whether you are a professional makeup artist or just love experimenting with makeup, at MirAy, you will surely find something special for yourself. Come visit us and let's create a world of beauty together, where everyone can feel confident and beautiful!
+						</p>
+
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default About;
